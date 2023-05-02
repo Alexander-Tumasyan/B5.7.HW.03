09359924ec588e19ed3852d33a3edebe6d7b4b62
Alexander Tumasyan <a.s.tumasyan@gmail.com>
2023-05-02 12:53:29 +0300

    edit files

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_version = "1.4.5"
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.89.0"
-    }
-  }
-  backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = var.bucket
-    region     = "ru-central1-a"
-    key        = "~/Terraform/B5.7.HW.03/terraform.tfstate"
-    access_key = var.access_key
-    secret_key = var.secret_key
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
-}
-
 provider "yandex" {
   token     = var.token
   cloud_id  = var.cloud_id
